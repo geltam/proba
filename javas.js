13e7bf1c56bd61c33728edd2a7127e743795ec27
Tamas <geltam11@gmail.com>
2020-09-23 22:51:46 +0200

    first commit

---
+++ javas.js
@@ -0,0 +1,13 @@
+function newpage(){
+    window.open("blog2.html");
+}
+
+function eger(x){
+    x.style.backgroundColor = "lightgray";
+    x.style.color = "black";
+}
+
+function eger1(x){
+    x.style.backgroundColor = "black";
+    x.style.color = "white";
+}
